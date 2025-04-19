aa3a60076aa316b2577684dd91f2e0621f4b3103
LoggysDev <albertmetsler3008@gmail.com>
2025-04-19 16:22:39 +0500

    Update Settings.c

--- LogSound/v1/logsound_firmware/structures/Settings.c
+++ LogSound/v1/logsound_firmware/structures/Settings.c
@@ -1,20 +1,27 @@
 #include "Settings.h"
 
 Settings setts = {
-    .ancEnabled = true,
-    .transparencyMode = false,
-    .autoSwitchInput = true,
-    .monoModeWhenOneCupOff = true,
-    .ancStrength = 75,
-    .transparencyLevel = 50,
-    .bassBoost = 0,
-    .trebleBoost = 0,
-    .ledEnabled = true,
-    .ledBrightness = 128,
-    .ledColor = 0x00FFAA,
-    .displayEnabled = true,
-    .displayTimeout = 15,
-    .autoPauseOnRemove = true,
-    .gestureControlEnabled = true,
-    .profileName = "User"
+    volume = 100,
+    ancEnabled = true,        
+    transparencyMode = false,       
+
+    autoSwitchInput = true,        
+    monoModeWhenOneCupOff = true,   
+
+    ancSpectre = 1;      
+    bassBoost;            
+    trebleBoost;           
+
+     ledEnabled = true;       
+     isVariableBrightness = true;
+     ledBrightness;    
+     ledVarBrightness;     
+     flashMode;            
+
+     autoPauseOnRemove;       
+     gestureControlEnabled;      
+     isMixStereoOnHalfRemove;   
+     isDownVolInBowl;          
+
+     profileName[16];        
 };
